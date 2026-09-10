efdf9d8ca2d2e3fb6821922f32f162352cb4dab1
klara98765 <nemckl@delta-studenti.cz>
2026-09-10 07:45:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    System.out.println("hi");
+    IO.println("Test me");
+
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        IO.println("i = "+ i);
+    }
+    IO.print("Goodbye");
+}
